bdb6d8f0820114c256a28d540f5bc06f92d47508
mdwork <mazdze@gmail.com>
2016-03-13 14:56:07 +0200

    mib

---
+++ js/mainJs.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.video-box').click(function(){
+        $('.video-box').html('</iframe><iframe width="100%" height="100%" src="https://www.youtube.com/embed/V6-0kYhqoRo?autoplay=1&loop=1&playlist=V6-0kYhqoRo" frameborder="0" allowfullscreen></iframe>');
+    });
+});
