be0e874ae935932f961dbc03716019da7b99f7bf
mdwork <mazdze@gmail.com>
2016-03-15 01:40:27 +0200

    mib

--- js/mainJs.js
+++ js/mainJs.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
     $('.video-box').click(function(){
         $('.video-box').html('</iframe><iframe width="100%" height="100%" src="https://www.youtube.com/embed/V6-0kYhqoRo?autoplay=1&loop=1&playlist=V6-0kYhqoRo" frameborder="0" allowfullscreen></iframe>');
     });
+
+    // for FAQ section
+    flag = true;
+    $(".question-head").on('click', function(){
+        if (flag == true) {
+            $(this).addClass('open');
+            flag = false;
+        } else {
+            $(this).removeClass('open');
+            flag = true;
+        }
+        $(this).siblings('.question-answer').slideToggle();
+    });
 });
