89bb41f5bc097c4773b081dcbc57c761dbae51a5
mdwork <mazdze@gmail.com>
2016-03-16 02:09:31 +0200

    mib

--- js/mainJs.js
+++ js/mainJs.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+    // for burger menu
+    $(".btn-toggle").on('click', function(){
+        $(this).parent().siblings('.toggle-box').slideToggle();
+    });
+
     // for video-block on Home Page
     $('.video-box').click(function(){
         $('.video-box').html('</iframe><iframe width="100%" height="100%" src="https://www.youtube.com/embed/V6-0kYhqoRo?autoplay=1&loop=1&playlist=V6-0kYhqoRo" frameborder="0" allowfullscreen></iframe>');
